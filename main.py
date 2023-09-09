75a5b09d0e366aef4a8ddbefc08742d8e3149a07
Marius2504 <marius.dumitrescu@s.unibuc.ro>
2023-09-09 22:11:43 +0300

    Adjusted parameters

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 
 def draw():
@@ -48,8 +49,8 @@ regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
 
-real_stock_price = dataset_train.iloc[2992 - 100:,6:7].values
-inputs = dataset_train.iloc[2992 - 100 - 60:, 6:7].values
+real_stock_price = dataset_train.iloc[2991 - 100:,6:7].values
+inputs = dataset_train.iloc[2991 - 100 - 60:, 6:7].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
 
@@ -61,8 +62,20 @@ X_test = np.array(X_test)
 X_test = X_test.astype(np.float32)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-#predict results
+
+
+
 predicted_stock_price = regressor.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
 draw()
+#predict results
+
+
+mse_test = mean_squared_error(real_stock_price, predicted_stock_price)
+mae_test = mean_absolute_error(real_stock_price, predicted_stock_price)
+r2_test = r2_score(real_stock_price, predicted_stock_price)
+
+print(f'Mean Squared Error (MSE) on Test Data: {mse_test:.4f}')
+print(f'Mean Absolute Error (MAE) on Test Data: {mae_test:.4f}')
+print(f'R-squared (R2) on Test Data: {r2_test:.4f}')
